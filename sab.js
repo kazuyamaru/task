a13069c2ab24b000b8aff89b875aa37147f74ac4
kazuyamaru <82344948+kazuyamaru@users.noreply.github.com>
2024-02-20 15:32:54 +0900

    Add files via upload

---
+++ sab.js
@@ -0,0 +1,21 @@
+$(window).scroll(function() {
+    const scrollPercentage = ($(window).scrollTop() / ($(document).height() - $(window).height())) * 100;
+    if (scrollPercentage > 20) {
+      $('header').css('background-color', '#210148');
+      $('header').css('transition', '0.3s');
+    } else {
+      $('header').css('background-color', 'transparent');
+    }
+  });
+
+  $(window).scroll(function() {
+    const scrollPercentage = ($(window).scrollTop() / ($(document).height() - $(window).height())) * 100;
+    if (scrollPercentage > 2) {
+      $('.circle').css('background-color', 'white');
+      $('.circle').css('transition', '0.5s');
+      $('.circle').css('border', 'solid 1px black');
+    } else {
+      $('.circle').css('background-color', 'transparent');
+      $('.circle').css('border', 'transparent');
+    }
+  });
